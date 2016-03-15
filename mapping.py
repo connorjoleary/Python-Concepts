2109325901a1d8fa24c55992da784c04a4df235f
fippy24 <connor.trumpet@gmail.com>
2016-03-15 16:56:01 -0600

    better reading
    still not workiing though

--- mapping.py
+++ mapping.py
@@ -1,5 +1,7 @@
 # Connor O'Leary
 # Joe Pagi
+import numpy as np
+
 
 file = open('city1.txt', 'r')
 file.readline()
@@ -7,10 +9,16 @@ file.readline()
 file.readline()
 file.readline() #reads in line titles
 file.readline()
+i=1
+file.readline()
+data = file.readline().split('\t')
 
 for line in file:
-    print line
-
-
-
+    if(len(line.split('\t'))==1):
+        i=i+1
+        j=0
+    else:
+        data[i].append(line.split('\t'))
+        j=j+1
+        print data[i][j]
 file.close()
