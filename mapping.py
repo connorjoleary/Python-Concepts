1d38d3441b249e8e9e6eaabd186e6d0239d9decd
fippy24 <connor.trumpet@gmail.com>
2016-03-17 22:19:08 -0600

    task 3

--- mapping.py
+++ mapping.py
@@ -11,32 +11,32 @@ file.readline() #reads in line titles
 file.readline()
 file.readline()
 for line in file:
-	clean = re.match('(\w+\s\w*\s\w*)\s+(\w+\s\w*\s\w*)\s+(\d+)', line)
-	if clean:
-		if clean.group(2).rstrip() not in final_map:
-			final_map[clean.group(2).rstrip()] = {clean.group(1).rstrip() : clean.group(3).rstrip()}
-		if clean.group(1).rstrip() not in final_map:
-			final_map[clean.group(1).rstrip()] = {clean.group(2).rstrip() : clean.group(3).rstrip()}
-		else:
-			final_map[clean.group(1).rstrip()].update({clean.group(2).rstrip() : clean.group(3).rstrip()})
-			final_map[clean.group(2).rstrip()].update({clean.group(1).rstrip() : clean.group(3).rstrip()})
+        clean = re.match('(\w+\s\w*\s\w*)\s+(\w+\s\w*\s\w*)\s+(\d+)', line)
+        if clean:
+                if clean.group(2).rstrip() not in final_map:
+                        final_map[clean.group(2).rstrip()] = {clean.group(1).rstrip() : clean.group(3).rstrip()}
+                if clean.group(1).rstrip() not in final_map:
+                        final_map[clean.group(1).rstrip()] = {clean.group(2).rstrip() : clean.group(3).rstrip()}
+                else:
+                        final_map[clean.group(1).rstrip()].update({clean.group(2).rstrip() : clean.group(3).rstrip()})
+                        final_map[clean.group(2).rstrip()].update({clean.group(1).rstrip() : clean.group(3).rstrip()})
 file.close()
 #print(final_map["  "])
 def task_1(city):
-	i = 0
-	for node in final_map[city]: 
-		i += 1;
-	print(i)
+        i = 0
+        for node in final_map[city]: 
+                i += 1;
+        print(i)
 #this works
 print 'task 1'
 city = raw_input('Enter the city: ')
 task_1(city)
 
 def task_2(city1, city2):
-	if city2 in final_map[city1]:
-		print("Yes")
-	else:
-		print("No")
+        if city2 in final_map[city1]:
+                print("Yes")
+        else:
+                print("No")
 #this works
 
 print 'task 2'
@@ -45,35 +45,42 @@ city2 = raw_input('Enter the city: ')
 task_2(city1, city2)
 #Task 3 is below function
 def find_connections(start, end, d, path=[]):
-	path = path + [start]
-	if start == end:
-		return path
-	if start not in final_map:
-		return None
-	for node in final_map[start]:
-		if node not in path:
-			newpath = find_path(node, end, path)
-			if newpath:
-				return newpath
-	return None
+        path = path + [start]
+        print start
+        if start == end:
+                return path
+        if start not in final_map or len(path)>d:
+                return None
+        for node in final_map[start]:
+                if node not in path:
+                        newpath = find_connections(node, end, d, path)
+                        if newpath:
+                                return newpath
+        return None
+path = find_connections('Anaconda', 'Choteau', 4)
+print 'task 3'
+print(path)
+
 
 #Task 4 is below function
 def find_path(start, end, path=[]):
-	path = path + [start]
-	if start == end:
-		return path
-	if start not in final_map:
-		return None
-	for node in final_map[start]:
-		if node not in path:
-			newpath = find_path(node, end, path)
-			if newpath:
-				return newpath
-	return None
+        path = path + [start]
+        if start == end:
+                return path
+        if start not in final_map:
+                return None
+        for node in final_map[start]:
+                if node not in path:
+                        newpath = find_path(node, end, path)
+                        if newpath:
+                                return newpath
+        return None
 d = 0
-path = find_path('Bozeman  ', 'Billings  ')
+print 'task 4'
+path = find_path('Bozeman', 'Billings')
 for index in range(len(path)):
-	if index >= 1:
-		d += int(final_map[path[index-1]][path[index]])
+        if index >= 1:
+                d += int(final_map[path[index-1]][path[index]])
+
 print(path)
 print(d)
